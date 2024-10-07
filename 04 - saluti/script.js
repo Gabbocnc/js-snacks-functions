dbdaeeebbe0be56ecc6e43624d6ffb04c5b8759c
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-07 15:31:16 +0200

    task 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const name = 'Mario';
-
-
+let salutare = salutaMario(name)
 // Dichiara la funzione qui.
-
+function salutaMario(nome){
+    return `Ciao ${name}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(salutare);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
