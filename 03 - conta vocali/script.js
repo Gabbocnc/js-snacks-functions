b8c1715a1bff65ca4eaef13d4db54f65d29d6f52
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-07 15:03:12 +0200

    task 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,9 +1,20 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
-
+let vocali = 'aeiou'
+let count = 0;
+let result = getVocali(word)
 // Dichiara la funzione qui.
+function getVocali(word){
+    for (let i = 0; i < word.lenght; i++){
+        
+        if (vocali.includes(word[i])) {
+            count++;
+        }
+    }
+    return count
+}
+console.log(result);
 
 
 // Invoca la funzione qui e stampa il risultato in console
